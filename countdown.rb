9404b1c1fea420c25278466a333824d61d6f221b
Eli Cleveland <elicleveland12@gmail.com>
2018-12-31 21:06:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,8 +10,8 @@ end
 
 def countdown_with_sleep
     while number > 0
-    puts "#{number} SECOND(S)!"
     sleep 1
+    puts "#{number} SECOND(S)!"
     number -= 1 
   end 
   return "HAPPY NEW YEAR!"
